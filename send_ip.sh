1c05bfcc3dba5361a29211baf3453567d9a167e3
cceasy <cceasy@hotmail.com>
2018-08-21 09:08:29 +0000

    update ip

---
+++ send_ip.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+from_name="raspberry pi"
+from="pi@raspberry.com"
+to_name="cceasy"
+to="jiahao.liu@hulu.com"
+subject="Raspberry Pi's IP"
+content="$(ifconfig | grep netmask | sed 's/^[ \t]*//g' | cut -d ' ' -f2)"
+previous_content="$(cat /home/pi/cron/myip.txt)"
+if [ "$content"x != "$previous_content"x ]; then
+echo $content > /home/pi/cron/myip.txt
+echo -e "To: \"${to_name}\" <${to}>\nFrom: \"${from_name}\" <${from}>\nSubject: ${subject}\n\n${content}" | /usr/sbin/sendmail -t
+fi
+
