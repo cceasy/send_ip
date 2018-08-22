13cfc60f0ced567687d5813d243038629dcebb8a
cceasy <cceasy@hotmail.com>
2018-08-22 09:39:03 +0000

    update ip

--- send_ip.sh
+++ send_ip.sh
@@ -9,9 +9,9 @@ content="$(ifconfig | grep netmask | sed 's/^[ \t]*//g' | cut -d ' ' -f2)"
 echo $content > tmp
 content=$(cat tmp)
 previous_content=$(cat myip.txt)
+rm -f tmp
 if [ "$content"x != "$previous_content"x ]; then
 echo $content > myip.txt
-rm -f tmp
 git add . && git commit -m 'update ip' && git push origin master
 echo -e "To: \"${to_name}\" <${to}>\nFrom: \"${from_name}\" <${from}>\nSubject: ${subject}\n\n${content}" | /usr/sbin/sendmail -t
 fi
