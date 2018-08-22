01ef3112b6516706f98b8b7f9b588a3ec73471de
cceasy <cceasy@hotmail.com>
2018-08-22 08:32:39 +0000

    update ip

--- send_ip_by_git.sh
+++ send_ip_by_git.sh
@@ -1,4 +1,4 @@
-content="$(ifconfig | grep netmask | sed -e 's/^[ \t]*//g' -e 's/\n/ /g' | cut -d ' ' -f2)"
+content="$(ifconfig | grep netmask | sed -e 's/^[ \t]*//g' -e 's/\n/#/g' | cut -d ' ' -f2)"
 previous_content=$(cat /home/pi/cron/send_ip/myip.txt)
 if [ "$content"x != "$previous_content"x ]; then
 echo "+$content+$previous_content+"
