69f6d630b1d0eb9cd957436226c5feedc1f5bc59
cceasy <cceasy@hotmail.com>
2018-08-22 08:38:02 +0000

    update ip

--- send_ip_by_git.sh
+++ send_ip_by_git.sh
@@ -1,7 +1,10 @@
-content="$(ifconfig | grep netmask | sed -e 's/^[ \t]*//g' -e 's/\n/#/g' | cut -d ' ' -f2)"
-previous_content=$(cat /home/pi/cron/send_ip/myip.txt)
+cd /home/pi/cron/send_ip
+content="$(ifconfig | grep netmask | sed -e 's/^[ \t]*//g' | cut -d ' ' -f2)"
+echo $content > tmp
+content=$(cat tmp)
+previous_content=$(cat myip.txt)
 if [ "$content"x != "$previous_content"x ]; then
-echo "+$content+$previous_content+"
-echo $content > /home/pi/cron/send_ip/myip.txt
-echo "$(cd /home/pi/cron/send_ip && git add . && git commit -m 'update ip' && git push origin master)"
+echo $content > myip.txt
+rm tmp
+echo "$(git add . && git commit -m 'update ip' && git push origin master)"
 fi
