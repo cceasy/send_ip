2f10acf315806d6f74284e2d2d72f3ded86489ff
cceasy <cceasy@hotmail.com>
2018-08-23 03:25:01 +0000

    update ip

--- send_ip.sh
+++ send_ip.sh
@@ -11,7 +11,9 @@ content=$(cat tmp)
 previous_content=$(cat myip.txt)
 rm -f tmp
 if [ "$content"x != "$previous_content"x ]; then
-echo "Raspberry Pi's IP:\n$content \n"
+echo "Raspberry Pi's IP:"
+echo $content
+echo ""
 echo $content > myip.txt
 git add . && git commit -m 'update ip' && git push origin master
 echo -e "To: \"${to_name}\" <${to}>\nFrom: \"${from_name}\" <${from}>\nSubject: ${subject}\n\n${content}" | /usr/sbin/sendmail -t
