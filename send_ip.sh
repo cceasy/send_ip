109063cda43c48a1da13284d78363c9e7d2135fc
cceasy <cceasy@hotmail.com>
2018-08-23 02:51:25 +0000

    update ip

--- send_ip.sh
+++ send_ip.sh
@@ -3,14 +3,17 @@ cd /home/pi/cron/send_ip
 from_name="raspberry pi"
 from="pi@raspberry.com"
 to_name="cceasy"
-to="jiahao.liu@hulu.com"
+to="cceasy@hotmail.com"
 subject="New Raspberry Pi's IP"
 content="$(ifconfig | grep netmask | sed 's/^[ \t]*//g' | cut -d ' ' -f2)"
 echo $content > tmp
 content=$(cat tmp)
 previous_content=$(cat myip.txt)
 rm -f tmp
+echo $content
 if [ "$content"x != "$previous_content"x ]; then
+echo $content
+echo ""
 echo $content > myip.txt
 git add . && git commit -m 'update ip' && git push origin master
 echo -e "To: \"${to_name}\" <${to}>\nFrom: \"${from_name}\" <${from}>\nSubject: ${subject}\n\n${content}" | /usr/sbin/sendmail -t
